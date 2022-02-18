ecaccd9466d8f20cdbda25d98675b4f2637cd6a2
Matheus <matheus@cda.lan>
2022-02-18 17:19:27 -0300

    create GuildIcon

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,13 +1,29 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
-import { Profile } from '../../components/Profile';
+import { CategorySelect } from '../../components/CategorySelect';
+import { ListHeader } from '../../components/ListHeader';
 import { ButtonAdd } from '../../components/ButtonAdd';
+import { Profile } from '../../components/Profile';
+import { FlatList, View } from 'react-native';
 
 import { styles } from './styles';
-import { CategorySelect } from '../../components/CategorySelect';
 
 export function Home() {
   const [category, setCategory] = useState("");
+
+  const appointments = [
+    {
+      id: '1',
+      guid: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true
+      },
+      category: '1',
+      date: '20/02 ás 20:40',
+      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+    }
+  ]
 
   function handleCategorySelect(categoryId) {
     categoryId === category ? setCategory("") : setCategory(categoryId);
@@ -25,6 +41,23 @@ export function Home() {
           categorySelected={category}
           setCategory={handleCategorySelect}
         />
+
+        <View style={styles.content}>
+
+          <ListHeader
+            title="Partidas Agendadadas"
+            subtitle="Total "
+          />
+
+          <FlatList
+            data={appointments}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+              <Text> { item.guid.name } </Text>
+            )}
+          />
+
+        </View>
       </View>
     </View>
   )
