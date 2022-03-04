6133ecd64dbb40fdf959177e147f8619774e1cef
Matheus <matheus@cda.lan>
2022-03-04 17:40:50 -0300

    Init to create hooks

--- App.js
+++ App.js
@@ -1,33 +1,49 @@
 import React from "react";
-import { Rajdhani_500Medium, Rajdhani_700Bold } from "@expo-google-fonts/rajdhani";
-import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import {
+	Rajdhani_500Medium,
+	Rajdhani_700Bold,
+} from "@expo-google-fonts/rajdhani";
+import {
+	Inter_400Regular,
+	Inter_500Medium,
+} from "@expo-google-fonts/inter";
 import AppLoading from "expo-app-loading";
 import { StatusBar } from "react-native";
 import { useFonts } from "expo-font";
+
+import { AuthContext } from "./src/hooks/auth";
 
 import { Routes } from "./src/routes/index";
 import { Background } from "./src/components/Background";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Inter_400Regular,
-    Inter_500Medium,
-    Rajdhani_500Medium,
-    Rajdhani_700Bold
-  });
+	const [fontsLoaded] = useFonts({
+		Inter_400Regular,
+		Inter_500Medium,
+		Rajdhani_500Medium,
+		Rajdhani_700Bold,
+	});
 
-  if(!fontsLoaded) {
-    return <AppLoading/>
-  }
+	if (!fontsLoaded) {
+		return <AppLoading />;
+	}
 
-  return (
-    <Background>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <Routes/>
-    </Background>
-  );
+	return (
+		<Background>
+			<StatusBar
+				barStyle='light-content'
+				backgroundColor='transparent'
+				translucent
+			/>
+			<AuthContext.Provider
+				value={{
+					name: "Matheus",
+					email: "matheuseugenio212@gmail.com",
+					avatar: "matheus.png",
+				}}
+			>
+				<Routes />
+			</AuthContext.Provider>
+		</Background>
+	);
 }
--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,53 +1,52 @@
-import React, { useState } from "react";
-import {
-  View,
-  Text,
-  Image,
-} from "react-native";
+import React, { useContext } from "react";
+import { View, Text, Image } from "react-native";
 
 import Illustration from "../../img/illustration.png";
 import { styles } from "./styles";
+
+import { AuthContext } from "../../hooks/auth";
 
 import { ButtonIcon } from "../../components/ButtonIcon/index";
 import { Background } from "../../components/Background";
 import { useNavigation } from "@react-navigation/native";
 
 export function SignIn() {
-  const Navigation = useNavigation();
+	const Navigation = useNavigation();
 
-  function handleSignIn(){
-    Navigation.navigate("Home");
-  }
+	const context = useContext(AuthContext);
+	console.log(context);
 
-  return (
-    <Background>
-      <View style={styles.container}>
+	function handleSignIn() {
+		Navigation.navigate("Home");
+	}
 
-        <Image
-          source={Illustration}
-          style={styles.imagem}
-          resizeMode='stretch'
-        />
+	return (
+		<Background>
+			<View style={styles.container}>
+				<Image
+					source={Illustration}
+					style={styles.imagem}
+					resizeMode='stretch'
+				/>
 
-        <View style={styles.content}>
-          <Text style={styles.title}>
-            Conecte-se {'\n'}
-            e organize suas {'\n'}
-            jogatinas
-          </Text>
+				<View style={styles.content}>
+					<Text style={styles.title}>
+						Conecte-se {"\n"}e organize suas {"\n"}
+						jogatinas
+					</Text>
 
-          <Text style={styles.subtitle}>
-            Crie grupos para jogar seus games {'\n'}
-            favoritos com seus amigos
-          </Text>
+					<Text style={styles.subtitle}>
+						Crie grupos para jogar seus games {"\n"}
+						favoritos com seus amigos
+					</Text>
 
-          <ButtonIcon
-            title="Entrar com o Discord"
-            activeOpacity={0.7}
-            onPress={handleSignIn}
-          />
-        </View>
-      </View>
-    </Background>
-  );
+					<ButtonIcon
+						title='Entrar com o Discord'
+						activeOpacity={0.7}
+						onPress={handleSignIn}
+					/>
+				</View>
+			</View>
+		</Background>
+	);
 }
